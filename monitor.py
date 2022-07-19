4eb9d1c0774d52f1f4f480a02cf520d37556d2d9
ius <50939413+xvhuan@users.noreply.github.com>
2022-07-19 16:34:44 +0800

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -26,25 +26,21 @@ def Monitor(challenge_id):
     if len(json_aim["data"]) < 3:
         if len(json_aim["data"]) == 0:
             name = parse.quote("恭喜" + user_name + "拿下《" + json_name + "》一血！！！太强啦~")
-            requests.get(
-                url="http://{}/MyQQHTTPAPI?function=Api_SendMsg&token={}&c1={}&c2=2&c3={}&c4=&c5={}".format(
-                    api, token,
-                    robot, group, name))
+            json_d = {"sessionKey": token, "target": int(group),
+                    "messageChain": [{"type": "Plain", "text": parse.unquote(name)}]}
+            requests.post(url="http://{}/sendGroupMessage".format(api), json=json_d)
         if len(json_aim["data"]) == 1:
             name = parse.quote("恭喜" + user_name + "拿下《" + json_name + "》二血！！太秀啦~")
-            requests.get(
-                url="http://{}/MyQQHTTPAPI?function=Api_SendMsg&token={}&c1={}&c2=2&c3={}&c4=&c5={}".format(
-                    api, token,
-                    robot, group, name))
+            json_d = {"sessionKey": token, "target": int(group),
+                    "messageChain": [{"type": "Plain", "text": parse.unquote(name)}]}
+            requests.post(url="http://{}/sendGroupMessage".format(api), json=json_d)
         if len(json_aim["data"]) == 2:
             name = parse.quote("恭喜" + user_name + "拿下《" + json_name + "》三血！666")
-            requests.get(
-                url="http://{}/MyQQHTTPAPI?function=Api_SendMsg&token={}&c1={}&c2=2&c3={}&c4=&c5={}".format(
-                    api, token,
-                    robot, group, name))
+            json_d = {"sessionKey": token, "target": int(group),
+                    "messageChain": [{"type": "Plain", "text": parse.unquote(name)}]}
+            requests.post(url="http://{}/sendGroupMessage".format(api), json=json_d)
         if 3 < len(json_aim["data"]) < 5:
             name = parse.quote("恭喜" + user_name + "拿下《" + json_name + "》，太强啦~")
-            requests.get(
-                url="http://{}/MyQQHTTPAPI?function=Api_SendMsg&token={}&c1={}&c2=2&c3={}&c4=&c5={}".format(
-                    api, token,
-                    robot, group, name))
+            json_d = {"sessionKey": token, "target": int(group),
+                    "messageChain": [{"type": "Plain", "text": parse.unquote(name)}]}
+            requests.post(url="http://{}/sendGroupMessage".format(api), json=json_d)
